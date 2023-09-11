36c60ee9370866068758219841f94b7cee385b9f
Pappu <pappubishwas>
2023-09-11 16:27:47 +0530

    Is lucky , check a number

---
+++ isLuckyNumber.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def isLucky(self, n): 
+        
+        i = 2
+    
+        for i in range(2, n+1):
+            if n % i == 0:
+                return False
+            n = n - n // i
+        
+        return True
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+
+if __name__ == '__main__':
+    t=int(input())
+    
+    for tcs in range(t):
+        n=int(input())
+        obj = Solution()
+        if obj.isLucky(n):
+            print(1)
+        else:
+            print(0)
+        
+# } Driver Code Ends
