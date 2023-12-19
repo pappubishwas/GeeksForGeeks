9f80f7997b926c0c8930520ed6369b8cd918ec99
pappubishwas <pappuovi8@gmail.com>
2023-12-19 10:36:43 +0530

    Updated files

---
+++ rightMostDifferentBit.py
@@ -0,0 +1,52 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+
+    
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the first position with different bits.
+    def posOfRightMostDiffBit(self,m,n):
+        for i in range(32):
+            if (m>>i & 1)!=(n>>i & 1):
+                return i+1
+        return -1        
+        #Your code here
+
+#{ 
+ # Driver Code Starts.
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        mn=[int(x) for x in input().strip().split()]
+        m=mn[0]
+        n=mn[1]
+        ob=Solution()
+        print(math.floor(ob.posOfRightMostDiffBit(m,n)))
+        
+        
+        
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
