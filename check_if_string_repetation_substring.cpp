6e31c24d9c959908a0c6cdf4d1d4d3a2a4f4db50
pappubishwas <pappuovi8@gmail.com>
2023-12-29 21:55:34 +0530

    updated

---
+++ check_if_string_repetation_substring.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    int kSubstrConcat(int n, string s, int k) {
+        if (n % k != 0) return 0;
+        
+        set<string> substrings;
+        map<string, int> substringCount;
+
+        for (int i = 0; i < n; i += k) {
+            string currentSubstr = s.substr(i, k);
+            substrings.insert(currentSubstr);
+            substringCount[currentSubstr]++;
+        }
+
+        if (substrings.size() > 2) return 0; 
+        if (substrings.size() == 2) {
+            auto it = substrings.begin();
+            if (substringCount[*it] > 1) {
+                it++;
+                if (substringCount[*it] > 1) return 0; 
+            }
+        }
+
+        return 1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		int n; cin >> n;
+		string s; cin >> s;
+		int k; cin >> k;
+        Solution  ob;
+		cout << ob.kSubstrConcat (n, s, k) << endl;
+	}
+}
+// } Driver Code Ends
