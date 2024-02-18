70cc6cdbf168de89ba57a7e21353d76e5ad63624
pappubishwas <pappuovi8@gmail.com>
2024-02-19 01:31:14 +0530

    updated

---
+++ does_array_represent_heap.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool isMaxHeap(int arr[], int n)
+    {
+        // Your code goes here
+        for(int i=0;i<n;i++){
+            int l=2*i+1;
+            int r=2*i+2;
+            if(l<n && arr[i]<arr[l]) return 0;
+            if(r<n && arr[i]<arr[r]) return 0;
+        }
+        return 1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+       int n;
+       cin >> n;
+       int a[4*n]={0};
+       for(int i =0;i<n;i++){
+           cin >> a[i];
+       }
+       Solution ob;
+       cout<<ob.isMaxHeap(a, n)<<endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
